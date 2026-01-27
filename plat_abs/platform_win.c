18a5ea10400ceb3f2d19fb8134fc26a6511925c5
entercrystal <217840228+entercrystal@users.noreply.github.com>
2026-01-27 16:34:54 +0200

    Windows Platform Abstraction

---
+++ plat_abs/platform_win.c
@@ -0,0 +1,34 @@
+// PLATFORM ABSTRACTION FOR WINDOWS [WIN 10/11 ONLY]
+#include <stdbool.h>
+#include <windows.h>
+
+void EnableANSI() {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(hOut, &cursorInfo);
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(hOut, &cursorInfo);
+}
+
+void ClearConsole() {
+    system("cls");
+}
+
+bool GetConsoleSize(int *rows, int *columns) {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (!GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+        return false;
+    }
+    *columns = csbi.srWindow.Right - csbi.srWindow.Left;
+    *rows = csbi.srWindow.Bottom - csbi.srWindow.Top - 1;
+    return true;
+}
+
+void sleep(DWORD milliseconds) {
+    Sleep(milliseconds);
+}
